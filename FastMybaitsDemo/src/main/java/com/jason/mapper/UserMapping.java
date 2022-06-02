b802bfbecb80747c88ffa2ec572338b83aaa30b1
jason <qh404836248@163.com>
2022-06-02 16:16:50 +0800

    添加 FastMybatis、Mybatis Plus blog

---
+++ FastMybaitsDemo/src/main/java/com/jason/mapper/UserMapping.java
@@ -0,0 +1,9 @@
+package com.jason.mapper;
+
+import com.gitee.fastmybatis.core.mapper.CrudMapper;
+import com.jason.domain.User;
+
+//@Mapper
+public interface UserMapping extends CrudMapper<User, Integer>{
+
+}
---
+++ Mybaits-Plus-Demo/src/main/java/com/jason/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.jason.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+@Configuration
+@MapperScan(basePackages = "com.jason.mapper")
+public class MybatisPlusConfig {
+
+	/**
+	 * 分页插件
+	 */
+	@Bean
+	public MybatisPlusInterceptor paginationInnerInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+	}
+}
