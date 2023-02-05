361fa61ed8abd58a829e7973c9c2c308de690574
jason <qh404836248@163.com>
2023-02-05 19:04:28 +0800

    添加SpringDemo；添加aop拦截器计算方法耗时

---
+++ SpringbootDemo/src/main/java/com/qian/intercept/MethodRTIntercept.java
@@ -0,0 +1,27 @@
+package com.qian.intercept;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: 小小千千
+ * @CreateTime: 2023/2/5
+ * @Description: 在接口方法打印耗时日志
+ */
+@Component
+public class MethodRTIntercept implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        long startTs = System.currentTimeMillis();
+        try {
+            invocation.proceed();
+            long endTs = System.currentTimeMillis();
+            System.out.println("方法" + invocation.getMethod().getName() + "耗时：" + (endTs - startTs));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
